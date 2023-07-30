ec8d092b6589d86128170f5c54ea5f0cd66c56e9
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-07-30 17:49:04 +0300

    Refactor user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Post, Get, Body, Param, UsePipes, ConflictException, ForbiddenException, NotFoundException } from '@nestjs/common';
-import { ConfigService } from "@nestjs/config";
+import { Controller, Post, Get, Body, Param, UsePipes, Redirect, NotFoundException } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { JoiValidationPipe } from 'src/pipes/validation.pipe';
@@ -7,37 +6,24 @@ import { JoiValidationPipe } from 'src/pipes/validation.pipe';
 import { SignupReqBody } from 'src/interfaces/user.interface';
 import joiSignupUserSchema from 'src/schemas/user.joiSignupSchema';
 
-import sendMail from 'src/helpers/sendMail';
-
-
 @Controller('users')
 export class UsersController {
-    constructor(private usersService: UsersService, private configService: ConfigService) { }
+    constructor(private usersService: UsersService) { }
     
     @Post('signup')
         @UsePipes(new JoiValidationPipe(joiSignupUserSchema))
     async signup(@Body() user: SignupReqBody) {
-        const isUserInDB = await this.usersService.findUserByEmail(user.email);
-        if (isUserInDB) {
-            throw new ConflictException('This email already in use');
-        }
+        await this.usersService.findUserByEmail(user.email);
         const newUser = await this.usersService.createNewUser(user);
-        const sendgridKey = this.configService.get<string>('SENDGRID_KEY');
-        const isMailSent = await sendMail(sendgridKey, newUser.email, newUser.verificationToken);
-        if (typeof isMailSent === 'boolean') {
-            return newUser;
-        } else {
-            throw new ForbiddenException('Confirmation email was not sent');
-        }
+        await this.usersService.sendVerificationMail(newUser.email, newUser.verificationToken);
+        return newUser;
     }
 
     @Get('verify/:verificationToken')
+        @Redirect()
     async verify(@Param('verificationToken') verificationToken: string) {
-        const isUserInDB = await this.usersService.findUserByVerificationToken(verificationToken);
-        if (!isUserInDB) {
-            throw new NotFoundException('User not found');
-        }
-        const verifiedUser = await this.usersService.updateUserIsVerified(isUserInDB._id);
-        return verifiedUser;
+        const userWithToken = await this.usersService.findUserByVerificationToken(verificationToken);
+        const verifiedUser = await this.usersService.updateUserIsVerified(userWithToken._id);
+        return {url: 'https://www.youtube.com'};
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, ConflictException, ForbiddenException, NotFoundException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from "mongoose";
@@ -8,13 +9,17 @@ import { User } from "src/schemas/user.mongooseSchema";
 import { SignupReqBody } from "src/interfaces/user.interface";
 
 import hashPassword from 'src/helpers/hashPassword';
+import sendMail from "src/helpers/sendMail";
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User.name) private userModel: Model<User>, private jwtService: JwtService) { }
+    constructor(@InjectModel(User.name) private userModel: Model<User>, private jwtService: JwtService, private configService: ConfigService) { }
 
-    async findUserByEmail(email: string): Promise<boolean> {
-        return await this.userModel.findOne({ email });
+    async findUserByEmail(email: string): Promise<void> {
+        const user = await this.userModel.findOne({ email });
+        if (user) {
+throw new ConflictException('This email already in use');
+        } 
     }
 
     async createNewUser(user: SignupReqBody) {
@@ -24,8 +29,20 @@ export class UsersService {
         return newUser;
     }
 
+    async sendVerificationMail(email: string, token: string): Promise<void> {
+        const sendgridKey = this.configService.get<string>('SENDGRID_KEY');
+const isMailSent = await sendMail(sendgridKey, email, token);
+        if (typeof isMailSent !== 'boolean') {
+            throw new ForbiddenException('Confirmation email was not sent');
+        }
+    }
+
     async findUserByVerificationToken(verificationToken: string) {
-        return await this.userModel.findOne({ verificationToken });
+        const user = await this.userModel.findOne({ verificationToken });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
     }
 
     async updateUserIsVerified(userId: Types.ObjectId) {
