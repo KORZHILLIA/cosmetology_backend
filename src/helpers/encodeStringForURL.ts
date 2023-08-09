acee2004c19601fc595b255ccd8e3302102943b5
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-08-09 21:33:21 +0300

    Change devider at encode function

--- src/helpers/encodeStringForURL.ts
+++ src/helpers/encodeStringForURL.ts
@@ -2,7 +2,7 @@ const encodeStringForURL = (string: string): string => {
   const encodedArr = new Array(string.length)
     .fill('')
     .map((_, idx) => string.charCodeAt(idx));
-  return encodedArr.join('%');
+  return encodedArr.join('_');
 };
 
 export default encodeStringForURL;
